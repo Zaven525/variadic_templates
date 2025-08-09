72231a1ed39b1859a1bbfda007a756e9b2effdbb
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2025-08-09 18:25:37 +0400

    Add files via upload

--- variadic_templates.cpp
+++ variadic_templates.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 // template<typename ...Args>
 // void print(Args ...args) {
 //     ((std::cout << args), ...);
@@ -29,15 +31,75 @@
 // }
 
 
-template<template<typename> class Predicat, typename ...Types>
-struct count_if;
+// template<template<typename> class Predicat, typename ...Types>
+// struct count_if;
 
-template<template<typename> class Predicat>
-struct count_if<Predicat> {
-    enum { val = 0; }
-};
+// template<template<typename> class Predicat>
+// struct count_if<Predicat> {
+//     enum { val = 0; }
+// };
 
-template <template<typename> class Predicat, typename T, typename ...Types>
-struct count_if<Predicat, T, Types ...> {
-    enum {val = Pred<T>::value + count_if<Predicat, Types...>}::val;
-};
+// template <template<typename> class Predicat, typename T, typename ...Types>
+// struct count_if<Predicat, T, Types ...> {
+//     enum {val = Pred<T>::value + count_if<Predicat, Types...>}::val;
+// };
+
+
+template<typename First>
+void print_with_sep(std::string sep, First first) {
+    std::cout << first << std::endl;
+}
+
+template<typename First, typename ...Args>
+void print_with_sep(std::string sep, First first, Args...args) {
+    std::cout << first << sep;
+    print_with_sep(sep, args...);
+}
+
+std::string to_upper(const std::string& s) {
+    std::string res = s;
+    std::transform(res.begin(), res.end(), res.begin(), ::toupper);
+    return res;
+}
+
+template<typename First>
+void print_upper(std::string sep, First first) {
+    std::cout << to_upper(first) << std::endl;
+}
+
+template<typename First, typename ...Args>
+void print_upper(std::string sep, First first, Args ...args) {
+    std::cout << to_upper(first) << sep;
+    print_upper(sep, args...);
+}
+
+std::string compress(std::string str) {
+    std::string res;
+    for (char i : str) {
+        if (i != ' ') {
+            res += i;
+        }
+    }
+    return res;
+}
+
+
+template<typename First>
+void print_compressed(First first) {
+    std::cout << compress(first) << std::endl;
+}
+
+template<typename First, typename ...Args>
+void print_compressed(First first, Args ...args) {
+    std::cout << compress(first);
+    print_compressed(args...);
+}
+
+
+int main() {
+    print_with_sep(", ", "apple", "banan");
+    print_upper(", ", "apple", "banan");
+    print_compressed("Hello World", "hihi haha");
+
+    return 0;
+}
